6a0656415003c7899be6dca52a11184262cc9081
Abhishek Dilip Dhane <Abhishek Dilip Dhane@LAPTOP-IBLHU5QH>
2022-06-22 13:36:21 +0530

    Commented P and C

--- Abhishek Testing/src/pack123/replace.java
+++ Abhishek Testing/src/pack123/replace.java
@@ -7,15 +7,15 @@ public class replace
 	String a = "Velocity Testing Batch Pune City";
 	a= a.replace('V', 'v');
 	System.out.println(a);
-	
+	 
 	
 	a= a.replace('B', 'b');
 	System.out.println(a);
 	
-	a= a.replace('P', 'p');
-	System.out.println(a);
+	//a= a.replace('P', 'p');
+	//System.out.println(a);
 	
-	a= a.replace('C', 'c');
-	System.out.println(a);
+	//a= a.replace('C', 'c');
+	//System.out.println(a);
 	}
 }
